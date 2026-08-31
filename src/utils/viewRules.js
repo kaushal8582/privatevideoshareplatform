8ff92b9b11ea8e49e05bae45342dec3f1611f0a2
Anokhi-dev <kaushalk.tf@gmail.com>
2026-08-31 09:07:32 +0530

    Update view time

--- src/utils/viewRules.js
+++ src/utils/viewRules.js
@@ -1,5 +1,5 @@
 export const getViewRules = () => ({
   minCountedSeconds: Number(process.env.VIEW_COUNTED_SECONDS) || 5,
-  minPayableSeconds: Number(process.env.VIEW_PAYABLE_SECONDS) || 20,
+  minPayableSeconds: Number(process.env.VIEW_PAYABLE_SECONDS) || 60,
   usdPerThousand: Number(process.env.VIEW_USD_PER_1000) || 1,
 });
